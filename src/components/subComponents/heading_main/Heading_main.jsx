2dbbb21eaecd614778873ee0b66612889dad474b
Nwaodu Nkechukwu <nksoftwaredev@gmail.com>
2026-02-26 05:51:02 +0000

    Fix import statement for Heading_main.scss

--- src/components/subComponents/heading_main/Heading_main.jsx
+++ src/components/subComponents/heading_main/Heading_main.jsx
@@ -1,7 +1,8 @@
-import './Heading_main.scss';
+import './heading_main.scss';
 
 const Heading_main = ({children,className}) => {
   return <h2 className={className}>{children}</h2>;
 };
 
 export default Heading_main;
+
